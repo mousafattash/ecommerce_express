e555280f5cb03e3fc682de3295d6fd3581131b76
mousa <mousafattash407@gmail.com>
2025-04-03 01:54:49 +0200

    lec 7 done

---
+++ DB/models/order.model.js
@@ -0,0 +1,82 @@
+import { Schema, model,mongoose,Types } from 'mongoose';
+
+const orderSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    products: [
+        {
+            productName: {
+                type: String,
+                required: true,
+            },
+            product: {
+                type: Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true,
+                min: [1, 'Quantity must be at least 1']
+            },
+            unitPrice: {
+                type: Number,
+                required: true,
+                min: [0, 'Unit price cannot be negative']
+
+            },
+            totalPrice: {
+                type: Number,
+                required: true,
+                min: [0, 'Total price cannot be negative']
+            },
+        }
+    ],
+    couponName: {
+        type: String,
+    },
+    finalPrice: {
+        type: Number,
+        required: true,
+        min: [0, 'Final price cannot be negative']
+    },
+    paymentStatus: {
+        type: String,
+        enum: ['pending', 'completed', 'failed'],
+        default: 'pending'
+    },
+    paymentType: {
+        type: String,
+        enum: ['cash','credit_card'],
+    },
+    shippingAddress: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending','cancelled','confirmed', 'onWay', 'delivered'],
+        default: 'pending'
+    },
+    note: {
+        type: String,
+    },
+    reasonRejected: {
+        type: String,
+    },
+    updatedBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+}, { timestamps: true });
+
+
+
+
+
+const Order = mongoose.model('Order', orderSchema);
+export default Order;
